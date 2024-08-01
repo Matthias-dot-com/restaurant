b1a1c02e75697d301aab1c33af53a0f25180539a
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-01 11:57:53 +0400

    all configuration done sofar

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,18 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-
-  module :{
-    rules : [
-        new HtmlWebpackPlugin ({
-            title : "Restaurant",
-            template : "./src/index.html",
-        })
-    ]
-  }
+  module: {
+    rules: [
+      { test: /\.png$/i, type: "asset/resource" },
+     
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
 };
