07b7d90a367cad78e06f3b65773c60153292354f
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-01 15:06:17 +0400

    Homepage done

--- src/home.js
+++ src/home.js
@@ -1,9 +1,80 @@
-import Img from "./home.jpg";
+
 export default function createHome() {
-  const img = new Image();
-  img.src = Img;
   const div = document.createElement("div");
   div.classList.add("home");
-  div.appendChild(img);
+
+  const restaurantName = "Restaurant";
+  const restaurantNameContainer = document.createElement("div");
+  const h1 = document.createElement("h1");
+
+  h1.innerHTML = restaurantName;
+  h1.classList.add("top");
+  restaurantNameContainer.appendChild(h1);
+  const bottomLinks = ["Meat", "Vegan", "Soft-Drinks", "Alcohol"];
+  const bottom = document.createElement("div");
+  bottom.classList.add("bottom-links");
+  const uList = document.createElement("ul");
+  
+
+  bottomLinks.forEach((link) => {
+    const list = document.createElement("li");
+    list.classList.add(link);
+    list.textContent = link;
+    uList.appendChild(list);
+  });
+
+  bottom.appendChild(uList);
+  document.body.appendChild(restaurantNameContainer);
+  document.body.appendChild(bottom);
   return div;
 }
+// class HomePage {
+//   constructor() {
+//     this.div = document.createElement("div");
+//     this.div.classList.add("home");
+//     this.createContent();
+//   }
+
+//   createContent() {
+//     const fragment = document.createDocumentFragment();
+
+//     fragment.appendChild(this.createRestaurantNameContainer());
+//     fragment.appendChild(this.createBottomLinks());
+
+//     this.div.appendChild(fragment);
+//     document.body.appendChild(this.div);
+//   }
+
+//   createRestaurantNameContainer() {
+//     const container = document.createElement("div");
+//     const h1 = document.createElement("h1");
+
+//     h1.textContent = "Restaurant";
+//     h1.classList.add("top");
+
+//     container.appendChild(h1);
+//     return container;
+//   }
+
+//   createBottomLinks() {
+//     const bottomLinks = ["Meat", "Vegan", "Soft-Drinks", "Alcohol"];
+//     const bottom = document.createElement("div");
+//     bottom.classList.add("bottom-links");
+//     const uList = document.createElement("ul");
+
+//     bottomLinks.forEach((link) => {
+//       const list = document.createElement("li");
+//       list.classList.add(link);
+//       list.textContent = link;
+//       uList.appendChild(list);
+//     });
+
+//     bottom.appendChild(uList);
+//     return bottom;
+//   }
+// }
+
+// // To create and add the home page to the DOM
+// export default function createHome() {
+//   new HomePage();
+// }
